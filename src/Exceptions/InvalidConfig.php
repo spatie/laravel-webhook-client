951a3d91e944f99e1fd0aa1addbc6adaa1fa089c
freek <freek@spatie.be>
2019-06-13 22:37:45 +0200

    wip

--- src/Exceptions/InvalidConfig.php
+++ src/Exceptions/InvalidConfig.php
@@ -3,12 +3,36 @@
 namespace Spatie\WebhookClient\Exceptions;
 
 use Exception;
+use Spatie\WebhookClient\ProcessWebhookJob;
+use Spatie\WebhookClient\SignatureValidator\SignatureValidator;
+use Spatie\WebhookClient\WebhookProfile\WebhookProfile;
 
 class InvalidConfig extends Exception
 {
     public static function couldNotFindConfig(string $notFoundConfigName)
     {
         return new static("Could not find the configuration for `{$notFoundConfigName}`");
+    }
+
+    public static function invalidSignatureValidator(string $invalidSignatureValidator)
+    {
+        $signatureValidatorInterface = SignatureValidator::class;
+
+        return new static("`{$invalidSignatureValidator}` is not a valid signature validator class. A valid signature validator is a class that implements `{$signatureValidatorInterface}`.");
+    }
+
+    public static function invalidWebhookProfile(string $webhookProfile)
+    {
+        $webhookProfileInterface = WebhookProfile::class;
+
+        return new static("`{$webhookProfile}` is not a valid webhook profile class. A valid web hook profile is a class that implements `{$webhookProfileInterface}`.");
+    }
+
+    public static function invalidProcessWebhookJob(string $processWebhookJob)
+    {
+        $abstractProcessWebhookJob = ProcessWebhookJob::class;
+
+        return new static("`{$processWebhookJob}` is not a valid process webhook job class. A valid class should implement `{$abstractProcessWebhookJob}`.");
     }
 }
 
--- src/WebhookConfig.php
+++ src/WebhookConfig.php
@@ -2,6 +2,9 @@
 
 namespace Spatie\WebhookClient;
 
+use Spatie\WebhookClient\Exceptions\InvalidConfig;
+use Spatie\WebhookClient\WebhookProfile\WebhookProfile;
+
 class WebhookConfig
 {
     /** @var string */
@@ -33,12 +36,21 @@ class WebhookConfig
 
         $this->signatureHeaderName = $properties['signature_header_name'];
 
+        if (! is_subclass_of($properties['signature_validator'], SignatureValidator::class)) {
+            throw InvalidConfig::invalidSignatureValidator($properties['signature_validator']);
+        }
         $this->signatureValidator = app($properties['signature_validator']);
 
+        if (! is_subclass_of($properties['webhook_profile'], WebhookProfile::class)) {
+            throw InvalidConfig::invalidWebhookProfile($properties['webhook_profile']);
+        }
         $this->webhookProfile = app($properties['webhook_profile']);
 
         $this->webhookModel = $properties['webhook_model'];
 
+        if (! is_subclass_of($properties['process_webhook_job'], ProcessWebhookJob::class)) {
+            throw InvalidConfig::invalidProcessWebhookJob($properties['process_webhook_job']);
+        }
         $this->processWebhookJob = app($properties['process_webhook_job']);
     }
 }
