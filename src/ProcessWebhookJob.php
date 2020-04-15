d4c211e6ab87f336cda8e88898798b53f974b7f8
James <N3rdP1um23@users.noreply.github.com>
2020-04-14 23:51:07 -0400

    Removed variable type causing error
    https://github.com/spatie/laravel-stripe-webhooks/issues/57

--- src/ProcessWebhookJob.php
+++ src/ProcessWebhookJob.php
@@ -13,7 +13,7 @@ abstract class ProcessWebhookJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public WebhookCall $webhookCall;
+    public $webhookCall;
 
     public function __construct(WebhookCall $webhookCall)
     {
