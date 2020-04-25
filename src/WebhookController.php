4daffae3d0d260874f02a92db9dab2f2dbe9400d
GuntherDebrauwer <22586858+GuntherDebrauwer@users.noreply.github.com>
2020-04-25 11:59:03 +0200

    Create response inside processor, instead of controller

--- src/WebhookController.php
+++ src/WebhookController.php
@@ -8,8 +8,6 @@ class WebhookController
 {
     public function __invoke(Request $request, WebhookConfig $config)
     {
-        (new WebhookProcessor($request, $config))->process();
-
-        return $config->webhookResponse->respondToValidWebhookRequest($request, $config);
+        return (new WebhookProcessor($request, $config))->process();
     }
 }
