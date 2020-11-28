c7427379299a1cdfedf0262760eb2e6c33a1f927
freekmurze <freekmurze@users.noreply.github.com>
2020-11-28 23:44:18 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -26,9 +26,9 @@ class TestCase extends Orchestra
     {
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
-            'driver'   => 'sqlite',
+            'driver' => 'sqlite',
             'database' => ':memory:',
-            'prefix'   => '',
+            'prefix' => '',
         ]);
     }
 
